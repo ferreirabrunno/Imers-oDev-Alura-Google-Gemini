d8f248c634485e320c9e42e15a7ba639b7ec396f
Bruno Vicente <106943433+ferreirabrunno@users.noreply.github.com>
2024-09-08 00:34:46 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+    {
+        escritor : "Monteiro Lobato",
+        descricao : "Monteiro Lobato foi um escritor brasileiro, conhecido por seus livros infantis como o Sítio do Picapau Amarelo.",
+        link : "https://pt.wikipedia.org/wiki/Monteiro_Lobato",
+        tags: "monteiro lobato literatura infantil literatura brasileira infanto juvenil classicos da literatura autores brasileiros escritor brasileiro pedrinho emilia visconde de sabugosa sitiodedopey"
+    },
+
+    {
+        escritor : "J.K. Rowling",
+        descricao : "J.K. Rowling é uma escritora britânica, mundialmente famosa por criar a saga Harry Potter.",
+        link : "https://pt.wikipedia.org/wiki/J._K._Rowling",
+        tags: "jovem j k rowling harry potter mundo magico literatura infantil fantasia livros filmes saga"
+    },
+
+    {
+        escritor : "Guimarães Rosa",
+        descricao : "Guimarães Rosa foi um escritor brasileiro, conhecido por sua obra-prima Grande Sertão: Veredas.",
+        link : "https://pt.wikipedia.org/wiki/Guimaraes_Rosa",
+        tags: "brasileiro literatura brasileira literatura sertaneja escritor brasileiro classicos da literatura literatura moderna  grande sertão:veredas primeiras histórias"
+    }
+];
+
+ 
